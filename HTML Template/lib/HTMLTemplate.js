c37012a1ab2958afe6286b0ad2e457ef1c88c701
SardineFish <Sardine@live.cn>
2017-04-01 21:34:07 +0800

    update

--- HTML Template/lib/HTMLTemplate.js
+++ HTML Template/lib/HTMLTemplate.js
@@ -1,6 +1,9 @@
 ﻿window.HTMLTemplate = (function (HTMLTemplate)
 {
-    HTMLTemplate = HTMLTemplate || function () { };
+    HTMLTemplate = HTMLTemplate || function (dom)
+    {
+
+    };
     var globalTemplates = (function ()
     {
         var list = [];
@@ -39,6 +42,358 @@
         }
         return list;
     })();
+    function Template(dom)
+    {
+        this.dom = dom;
+        this.attr = [];
+        this.children = [];
+        this.dataSouceBinding = null;
+        this.present = document.createElement("div");
+        this.present.className = "HTMLTemplatePresentation";
+        this.present.id = dom.id;
+        this.present.template = this;
+
+        var dataSource = null;
+
+        var template = this;
+        Object.defineProperty(this, "dataSource", {
+            get: function ()
+            {
+                return dataSource;
+            },
+            set: function (value)
+            {
+                dataSource = value;
+                template.render(template.present, value);
+            }
+        });
+        Object.defineProperty(this.present, "dataSource", {
+            get: function ()
+            {
+                return dataSource;
+            },
+            set: function (value)
+            {
+                dataSource = value;
+                template.render(template.present, value);
+            }
+        });
+        
+        for (var i = 0; i <dom.attributes &&  dom.attributes.length; i++)
+        {
+            if (dom.attributes[i].name == "datasource")
+            {
+                this.dataSouceBinding = dom.attributes[i].value;
+            }
+            else
+            {
+                this.attr[this.attr.length] = new AttributeTemplate(dom.attributes[i]);
+            }
+        }
+        for (var i = 0; i < dom.childNodes.length; i++)
+        {
+            var child = dom.childNodes[i];
+            if (child.nodeName == "#text")
+            {
+                this.children[this.children.length] = new TextTemplate(child.wholeText);
+            }
+            else if (child.nodeName == "TEMPLATE")
+            {
+                this.children[this.children.length] = new Template(child);
+            }
+            else
+            {
+                this.children[this.children.length] = new NodeTemplate(child);
+            }
+        }
+        dom.parentNode.replaceChild(this.present, dom);
+    }
+    Template.prototype.render = function (presentNode,source)
+    {
+        presentNode.innerHTML = "";
+        if (source instanceof Array)
+        {
+            for (var idx = 0; idx < source.length; idx++)
+            {
+                var node = document.createElement("div");
+                for (var i = 0; i < this.attr.length; i++)
+                {
+                    node.setAttributeNode(this.attr[i].render(source[idx]));
+                }
+                node.classList.add("HTMLTemplateNode");
+                for (var i = 0; i < this.children.length; i++)
+                {
+                    if (this.children[i] instanceof Template)
+                    {
+                        var present = document.createElement("div");
+                        for (var j = 0; j < this.children[i].attr.length; j++)
+                        {
+                            present.setAttributeNode(this.children[i].attr[j].render(source[idx]));
+                        }
+                        present.className = "HTMLTemplatePresentation";
+                        node.appendChild(present);
+                        if (!this.children[i].dataSouceBinding)
+                            continue;
+                        var src = eval("source." + this.children[i].dataSouceBinding);
+                        this.children[i].render(present, src);
+                    }
+                    else if (this.children[i] instanceof TextTemplate)
+                    {
+                        var textNode = document.createTextNode(this.children[i].render(source[idx]));
+                        node.appendChild(textNode);
+                    }
+                    else
+                    {
+                        node.appendChild(this.children[i].render(source[idx]));
+                    }
+                }
+                this.present.appendChild(node);
+            }
+        }
+        else if (window.ObservableList && source instanceof ObservableList)
+        {
+            for (var idx = 0; idx < source.length; idx++)
+            {
+                var node = document.createElement("div");
+                for (var i = 0; i < this.attr.length; i++)
+                {
+                    node.setAttributeNode(this.attr[i].render(source[idx]));
+                }
+                node.classList.add("HTMLTemplateNode");
+                for (var i = 0; i < this.children.length; i++)
+                {
+                    if (this.children[i] instanceof Template)
+                    {
+                        var present = document.createElement("div");
+                        for (var j = 0; j < this.children[i].attr.length; j++)
+                        {
+                            present.setAttributeNode(this.children[i].attr[j].render(source[idx]));
+                        }
+                        present.className = "HTMLTemplatePresentation";
+                        node.appendChild(present);
+                        if (!this.children[i].dataSouceBinding)
+                            continue;
+                        var src = eval("source." + this.children[i].dataSouceBinding);
+                        this.children[i].render(present, src);
+                    }
+                    else if (this.children[i] instanceof TextTemplate)
+                    {
+                        var textNode = document.createTextNode(this.children[i].render(source[idx]));
+                        node.appendChild(textNode);
+                    }
+                    else
+                    {
+                        node.appendChild(this.children[i].render(source[idx]));
+                    }
+                }
+                this.present.appendChild(node);
+            }
+        }
+        else 
+        {
+            var node = document.createElement("div");
+            for (var i = 0; i < this.attr.length; i++)
+            {
+                node.setAttributeNode(this.attr[i].render(source));
+            }
+            node.classList.add("HTMLTemplateNode");
+            for (var i = 0; i < this.children.length; i++)
+            {
+                if (this.children[i] instanceof Template)
+                {
+                    var present = document.createElement("div");
+                    for (var j = 0; j < this.children[i].attr.length; j++)
+                    {
+                        present.setAttributeNode(this.children[i].attr[j].render(source));
+                    }
+                    present.className = "HTMLTemplatePresentation";
+                    node.appendChild(present);
+                    if (!this.children[i].dataSouceBinding)
+                        continue;
+                    var src = eval("source." + this.children[i].dataSouceBinding);
+                    this.children[i].render(present, src);
+                }
+                else if (this.children[i] instanceof TextTemplate)
+                {
+                    var textNode = document.createTextNode(this.children[i].render(source));
+                    node.appendChild(textNode);
+                }
+                else
+                {
+                    node.appendChild(this.children[i].render(source));
+                }
+            }
+            this.present.appendChild(node);
+        }
+        
+    }
+    Template.prototype.toString = function ()
+    {
+        return "<Template>";
+    }
+
+    function NodeTemplate(dom)
+    {
+        this.tag = dom.localName;
+        this.children = [];
+        this.attr = [];
+        
+        for (var i = 0; dom.attributes &&  i < dom.attributes.length; i++)
+        {
+            this.attr[this.attr.length] = new AttributeTemplate(dom.attributes[i]);
+        }
+        for (var i = 0; i < dom.childNodes.length; i++)
+        {
+            var child = dom.childNodes[i];
+            if (child.nodeName == "#text")
+            {
+                this.children[this.children.length] = new TextTemplate(child.wholeText);
+            }
+            else if (child.nodeName == "TEMPLATE")
+            {
+                this.children[this.children.length] = new Template(child);
+            }
+            else
+            {
+                this.children[this.children.length] = new NodeTemplate(child);
+            }
+        }
+
+
+    }
+    NodeTemplate.prototype.render = function (source)
+    {
+        var node = document.createElement(this.tag);
+        for (var i = 0; i < this.attr.length; i++)
+        {
+            node.setAttributeNode(this.attr[i].render(source));
+        }
+        for (var i = 0; i < this.children.length; i++)
+        {
+            if (this.children[i] instanceof Template)
+            {
+                var present = document.createElement("div");
+                for (var j = 0; j < this.children[i].attr.length; j++)
+                {
+                    present.setAttributeNode(this.children[i].attr[j].render(source));
+                }
+                present.className = "HTMLTemplatePresentation";
+                node.appendChild(present);
+                if (!this.children[i].dataSouceBinding)
+                    continue;
+                var src = eval("source." + this.children[i].dataSouceBinding);
+                this.children[i].render(present, src);
+            }
+            else if (this.children[i] instanceof TextTemplate)
+            {
+                var textNode = document.createTextNode(this.children[i].render(source));
+                node.appendChild(textNode);
+            }
+            else
+            {
+                node.appendChild(this.children[i].render(source));
+            }
+        }
+        return node;
+    }
+    NodeTemplate.prototype.toString = function ()
+    {
+        return "<" + this.tag + ">";
+    }
+
+    function AttributeTemplate(attr)
+    {
+        this.name = attr.name;
+        this.value = new TextTemplate(attr.value);
+        this.raw = attr.name + "=" + attr.value;
+    }
+    AttributeTemplate.prototype.render = function (source)
+    {
+        var attr = document.createAttribute(this.name);
+        attr.value = this.value.render(source);
+        return attr;
+    }
+    AttributeTemplate.prototype.toString = function ()
+    {
+        return this.raw;
+    }
+
+    function TextTemplate(text)
+    {
+        this.raw = text;
+        var reg = /\{[^\{\}]*\}/;
+        this.combine = [];
+        var part = "";
+        for (var i = 0; i < text.length; i++)
+        {
+            if (text.charAt(i) == "{" && i + 1 < text.length && text.charAt (i+1)!= "+")
+            {
+                if (part.length > 0)
+                {
+                    this.combine[this.combine.length] = part;
+                    part = "";
+                }
+                var bindText = "";
+                for (; i < text.length; i++)
+                {
+                    bindText += text.charAt(i);
+
+                    if (text.charAt(i) == "}" && i - 1 > 0 && text.charAt(i - 1) != "+")
+                    {
+                        break;
+                    }
+                }
+                var bind = new Binding(bindText);
+                this.combine[this.combine.length] = bind;
+            }
+            else
+            {
+                part += text.charAt(i);
+            }
+        }
+        if (part.length)
+        {
+            this.combine[this.combine.length] = part;
+        }
+    }
+    TextTemplate.prototype.render = function (source)
+    {
+        var text = "";
+        for (var i = 0; i < this.combine.length; i++)
+        {
+            if (this.combine[i] instanceof Binding)
+            {
+                text += this.combine[i].exec(source);
+            }
+            else
+                text += this.combine[i];
+        }
+        return text;
+    }
+    TextTemplate.prototype.toString = function ()
+    {
+        return this.raw;
+    }
+
+    function Binding(bindingText)
+    {
+        this.bind = "";
+
+        if (/\{([_0-9a-zA-Z]+)\}/.test(bindingText))
+        {
+            this.bind = /\{([_0-9a-zA-Z]+)\}/.exec(bindingText)[1];
+        }
+        else
+        {
+            var data = JSON.parse(bindingText);
+            this.bind = data.bind;
+        }
+    }
+    Binding.prototype.exec = function (obj)
+    {
+        var text = eval("obj." + this.bind);
+        return text;
+    }
     function Init(templateDOM)
     {
         //Init specified template
@@ -108,6 +463,9 @@
                             continue;
                         if (list[i].childNodes[j].localName == "template")
                         {
+                            var template = new Template(list[i].childNodes[j]);
+                            continue;
+                            var node=new Template(list[i].childNodes[j]);
                             templateList[templateList.length] = list[i].childNodes[j];
                         }
                         else
@@ -145,6 +503,8 @@
             var key = match[1];
             html = html.replace("{" + key + "}", obj[key]);
         }
+        html = html.replace("{+", "{");
+        html = html.replace("+}", "}");
         var div = document.createElement("div");
         div.className = "HTMLTemplateNode "+template.className;
         div.innerHTML = html;
